f8c270c7f69e3d2aaafc02123f7f68d00d302820
Stephen Guo <epicsteveoj@gmail.com>
2023-06-20 14:05:44 -0400

    setup end2end tests for docker

--- README.md
+++ README.md
@@ -5,8 +5,14 @@ psql --username=rust-recipe-gallery --dbname=recipe-gallery
 diesel migration redo
 
 # Frontend
-tailwindcss --input ./input.css --output ./style/output.css --watch
+tailwindcss --input ./input.css --output ./style/tailwind.css --watch
 cargo leptos watch
+cd end2end
+npm install
+npx playwright install
+# npx playwright install-deps
+cd ..
+cargo leptos end-to-end
 ```
 
 ```sql
--- rust-recipe-gallery-frontend/src/app.rs
+++ rust-recipe-gallery-frontend/src/app.rs
@@ -24,7 +24,7 @@ fn Home(cx: Scope) -> impl IntoView {
 
     // thanks to https://tailwindcomponents.com/component/blue-buttons-example for the showcase layout
     view! { cx,
-        <Title text="Leptos + Tailwindcss"/>
+        <Title text="Rust Recipe Gallery"/>
         <main>
             <div class="bg-gradient-to-tl from-blue-800 to-blue-500 text-white font-mono flex flex-col min-h-screen">
                 <div class="flex flex-row-reverse flex-wrap m-auto">
